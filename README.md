b4d4e531bb62aa3ccce2b8b66840de5d4e7cee76
Christian Stewart <heeler_cs@hotmail.com>
2021-08-19 12:23:20 -0500

    Removed nginx. SSL was getting complex, so I'm not using docker for nginx

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 ## Usage
 
+Note: **Before running these commands, make sure you properly setup your domain.**
+
+**This has been tested on a Digital Ocean droplet running Ubuntu 20.04.**
+
 1. Rename "example.env" to ".env" and update with the correct values.
 1. Update server_name in "./nginx/default.conf" with the correct URL.
-1. Run `docker-compose up --build`
+1. Run `chmod +x ./setup.sh` in the root directory
+1. Run `./setup.sh`
---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+echo ""
+echo "This script does two things:"
+echo ""
+echo "1. Installs nginx, creates a .conf file, and adds ssl."
+echo "2. Creates a service for the docker-compose file so it will run at startup."
+echo ""
+echo "ONLY RUN THIS IF YOU ARE ON A LINUX PRODUCTION DISTRO (UBUNTU 20.04),"
+echo "IF YOU ARE ROOT PRIVELAGES,"
+echo "AND IF YOU HAVE COMPLETED THE OTHER STEPS IN THE README."
+echo ""
+echo "Continue? (y/N)"
+
+read continue
+
+if [[ $continue == 'y' ]]; then
+    apt update
+    apt upgrade -y
+    cp ./config/docker-compose-dbs.service /etc/systemd/system/docker-compose-dbs.service
+    systemctl enable docker-compose-dbs
+    apt install nginx
+    cp ./config/nginx.conf /etc/nginx/conf.d/default.conf
+    snap install core
+    snap refresh core
+    snap install --classic certbot
+    certbot --nginx
+    reboot
+fi
